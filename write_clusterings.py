c36ac6d075fbd28a7ea4c26dbf90139565a1b7a6
rcharbey <r.charbey@gmail.com>
2018-02-13 13:38:27 +0100

    print name

--- write_clusterings.py
+++ write_clusterings.py
@@ -32,19 +32,19 @@ def compute_clustering(graph):
     return cluster_per_alter
         
 
-def main():
-    with open('../Data/clusters_per_alters.csv', 'w') as to_write:
-        csvw = csv.writer(to_write, delimiter = ';')
-        csvw.writerow(['ego','alter', 'cluster' ])        
-        
+def main():        
         for graph_name in os.listdir('../Data/Graphs'):
             ego = graph_name.split('.')[0]
             graph = Graph.Read_GML('../Data/Graphs/%s' % graph_name)
             
             clustering = compute_clustering(graph)
             
-            for alter in clustering:
-                csvw.writerow([ego, alter, clustering[alter]])
+            with open('../Data/clusters_per_alters/%s.csv' % ego, 'w') as to_write:
+                csvw = csv.writer(to_write, delimiter = ';')
+                csvw.writerow(['alter', 'cluster' ])                  
+        
+                for alter in clustering:
+                    csvw.writerow([alter, clustering[alter]])
                 
 if __name__ == '__main__':
     main()
