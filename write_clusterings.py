8c428e364d15786e64e1ad6b790ddb7c92c05d3b
rcharbey <r.charbey@gmail.com>
2018-02-13 12:37:47 +0100

    modif Data

--- write_clusterings.py
+++ write_clusterings.py
@@ -6,9 +6,10 @@ Created on Mon Feb 12 18:46:20 2018
 """
 
 from igraph import *
+import os
+import csv
 
-def clutering(ego):
-    graph = Graph.read_gml('../DATA/$s.gml' % ego)
+def compute_clutering(graph):
     clusters_list = graph.community_multilevel()
     _cc_list = graph.decompose()
     cluster_per_alter = {}
@@ -17,3 +18,23 @@ def clutering(ego):
         for alter in cc:
             cluster_per_alter[cc] = index
         index += 1
+        
+
+def main():
+    with open('../Data/clusters_per_alters.csv', 'w') as to_write:
+        csvw = csv.writer(to_write, delimiter = ';')
+        csvw.writerow(['ego','alter', 'cluster' ])        
+        
+        for graph_name in os.listdir('../Data/Graphs'):
+            ego = graph_name.split('.')[0]
+            graph = Graph.read_gml('../Data/Graphs/$s.gml' % graph_name)
+            
+            clustering = compute_clustering(graph)
+            
+            for alter in clustering:
+                csv.writerow([ego, alter, clustering[alter]])
+                
+if __name__ == '__main__':
+    main()
+        
+        
