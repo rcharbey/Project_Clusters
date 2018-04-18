54328c3a620d8ac7eae5cf6bd7e075ce18324057
Raphael <r.charbey@gmail.com>
2018-04-18 18:16:15 +0200

    break removed

--- clusters_per_status.py
+++ clusters_per_status.py
@@ -31,6 +31,8 @@ for ego in listdir('%s/data/three/' % home):
     
     print '%s/GALLERY/three/%s/Graphs/friends.gml' % (home, ego)
     graph = Graph.Read_GML('%s/GALLERY/three/%s/Graphs/friends.gml' % (home, ego))
+    if len(graph.vs) == 0:
+        continue
     if not 'name' in graph.vs[0].attribute_names():
         continue
     if not 'cluster' in graph.vs[0].attribute_names():
